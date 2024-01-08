cba40ec20049e2503a9e5d1dca6a745f845a7274
mjaruzel <mjaruzel@10g.pl>
2024-01-08 15:30:33 +0100

    feat: support async functions

--- server.js
+++ server.js
@@ -3,16 +3,18 @@
 var vm = Npm.require('vm');
 
 Meteor.methods({
-  'xolvio/backdoor': function (func, args) {
+  'xolvio/backdoor': async function (func, args) {
     check(func, String);
     check(args, Match.Optional(Array));
 
     try {
       const preparedFunc = vm.runInThisContext(
-        '(function (require) { return (' + func + '); })'
+          '(function (require) { return (' + func + '); })'
       ).call(null, meteorInstall());
+      const value = await preparedFunc.apply(global, args);
+
       return {
-        value: preparedFunc.apply(global, args)
+        value
       };
     } catch (error) {
       return {
